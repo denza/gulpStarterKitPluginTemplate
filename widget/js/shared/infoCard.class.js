5a9ae7d44a163b310a90401d026aa06e60a83a2a
Daniel Hindi <Daniel@MadainCorp.com>
2019-07-14 19:47:13 -0700

    init

---
+++ widget/js/shared/infoCard.class.js
@@ -0,0 +1,39 @@
+/**
+ * Created by:
+ * Description:
+ *
+ *
+ * note: this is not made into a base class to avoid transpiling
+ */
+
+
+class InfoCard {
+	/**
+	 * @type {boolean}
+	 */
+	isActive;
+	/**
+	 * @type {Date}
+	 */
+	createdOn;
+	/**
+	 * @type {string}
+	 */
+	createdBy;
+	/**
+	 * @type {Date}
+	 */
+	lastUpdatedOn;
+	/**
+	 * @type {string}
+	 */
+	lastUpdatedBy;
+	/**
+	 * @type {Date}
+	 */
+	deletedOn;
+	/**
+	 * @type {string}
+	 */
+	deletedBy;
+}
