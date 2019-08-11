352d9675f4ac9c5b83c53e96b71190ad0eacfc0d
Daniel Hindi <Daniel@MadainCorp.com>
2019-08-11 09:44:06 -0700

    add more functionality to sortable list

--- control/content/samples/sortableList/sortableListUI.js
+++ control/content/samples/sortableList/sortableListUI.js
@@ -1,19 +1,28 @@
 const sortableListUI= {
 	sortableList:null
 	,contrainer:null
-	,init(elementId) {
+	,data:null
+	/*
+		This method will call the datastore to pull a single object
+		it needs to have an array property called `items` each item need {title, imgUrl}
+	 */
+	,init(elementId,tag="content") {
+		this.tag=tag;
 		this.contrainer = document.getElementById(elementId);
 		this.contrainer.innerHTML="loading...";
-		buildfire.datastore.get("content", (e, obj) => {
-
+		buildfire.datastore.get(this.tag, (e, obj) => {
 			if (e) {
 				console.error(e);
 				this.contrainer.innerHTML="An error has occurred. Please contact your system admin.";
 			}
 			else if (obj && obj.data) {
+				this.data = obj.data;
 				if(!obj.data.items || obj.data.items.length == 0)
 					this.contrainer.innerHTML="No items have been added yet.";
-				sortableListUI.render(obj.data.items);
+				else {
+					this.contrainer.innerHTML="";
+					sortableListUI.render(obj.data.items);
+				}
 			}
 		});
 	}
@@ -21,6 +30,7 @@ const sortableListUI= {
 
 
 	, render(items) {
+		let t = this;
 		this.sortableList = new buildfire.components.SortableList(this.contrainer, items || []);
 
 		this.sortableList.onItemClick = (item) => {
@@ -34,11 +44,22 @@ const sortableListUI= {
 				}, function (e, data) {
 					if (e) console.error(e);
 					if (data.selectedButton.key == "y") {
-						//delete
-						callback(true);
+						let itemDeleted= t.data.items.splice(index,1);
+						t.sortableList.loadItems(t.data.items,false);
+						buildfire.datastore.save({$set:{items:t.data.items}},t.tag,e=>{
+							callback(e,itemDeleted);
+						});
+
 					}
 				}
 			);
 		};
+
+		this.sortableList.onOrderChange=(item, oldIndex, newIndex)=>{
+
+			let temp = this.data.items.splice(oldIndex,1)[0];
+			this.data.items.splice(newIndex,0,temp);
+			buildfire.datastore.save({$set:{items:this.data.items}},this.tag,()=>{});
+		}
 	}
 };
