e9f5234600a2d384d89fc56151bddce60fae5a9b
Daniel Hindi <Daniel@MadainCorp.com>
2019-09-27 13:48:04 -0700

    fix popup example with js

--- control/content/samples/subPage/subPage.js
+++ control/content/samples/subPage/subPage.js
@@ -1,9 +1,13 @@
 
 class subPage{
 	constructor(id){
-		this.container = document.getElement(id);
+		this.container = document.getElementById(id);
 		if(!this.container)throw "Sub Page ID not found";
 		if(!this.container.classList.contains("subPage"))throw "Sub Page doesnt have class [subPage]";
+
+		let closeButton = this.container.querySelector(".close-modal");
+		if(closeButton)
+			closeButton.onclick=()=>{this.container.classList.remove("activeDialog");};
 
 	}
 
