5536c1fc98c401341b6f25bdbd9453b359389a38
mikailbayram <mikailbosna98@gmail.com>
2019-11-11 15:37:52 +0100

    Sort functionality

--- control/content/samples/searchTable/searchTableConfig.js
+++ control/content/samples/searchTable/searchTableConfig.js
@@ -10,24 +10,28 @@ const searchTableConfig ={
 			,data:"${data.name.firstName} ${data.name.lastName}"
 			,type:"string"
 			,width:"150px"
+			,sortBy: 'name.firstName'
 		}
 		,{
 			header:"e-mail"
 			,data:"${data.email}"
 			,type:"string"
 			,width:"150px"
+			,sortBy: 'email'
 		}
 		,{
 			header:"Credits"
 			,data:"${data.credits}"
 			,type:"number"
 			,width:"50px"
+			,sortBy: 'credits'
 		}
 		,{
 			header:"Created On"
 			,data:"${ new Date(data.createdOn).toLocaleDateString()  }"
 			,type:"date"
 			,width:"100px"
+			,sortBy: 'createdOn'
 		}
 	]
 
